2e3fa79f06cab046ff1e49866965c65d8e89656b
Luca-Lucidera <82151241+Luca-Lucidera@users.noreply.github.com>
2022-03-25 12:08:20 +0100

    lezione 11

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final stringa = 'ciao!';
-    return Center(
-      child: Text(
-        stringa,
-        textDirection: TextDirection.ltr,
+    return Container(
+      color: Colors.green.shade300,
+      padding: const EdgeInsets.only(left: 30, top: 20),
+      child: Column(
+        children: [
+          Text(
+            "Ciao1",
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            "Ciao2",
+            textDirection: TextDirection.ltr,
+          ),
+        ],
       ),
     );
   }
